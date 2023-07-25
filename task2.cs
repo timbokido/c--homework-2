95037c9d9fce505fad883d38fb9d4942c7cdaf04
timbokido <39024286+timbokido@users.noreply.github.com>
2023-07-25 12:14:25 +0200

    Add files via upload

---
+++ task2.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int number = new Random().Next(10,40000);
+if (number < 100) {
+    Console.WriteLine(number);
+    Console.WriteLine("третей цифры нет");
+}
+if ((number >= 100) && (number < 1000)) {
+    int a = number % 10;
+    Console.WriteLine(number);
+    Console.WriteLine(a);
+}
+if ((number >= 1000) && (number < 10000)) {
+    int b = number % 100;
+    int a = b/10;
+    Console.WriteLine(number);
+    Console.WriteLine(a);
+}
+if (number >= 10000) {
+    int b = number % 1000;
+    int a = b/100;
+    Console.WriteLine(number);
+    Console.WriteLine(a);
+}
+// 10076
